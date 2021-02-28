78eb65a3ce29fc8fc6933bdd4e3bd3ec75974285
Sean Ciser <seanciser@gmail.com>
2021-02-28 02:35:31 -0500

    update

--- MCP3008.py
+++ MCP3008.py
@@ -1,14 +1,3 @@
-# First install spidev:
-# Enable SPI (sudo raspi-config)
-# $ sudo apt-get update 
-# $ sudo apt-get upgrade
-# $ sudo apt-get install python-dev
-# $ sudo reboot
-# $ wget https://github.com/doceme/py-spidev/archive/master.zip 
-# $ unzip master.zip
-# $ cd py-spidev-master
-# $ sudo python setup.py install
-
 from spidev import SpiDev
 
 class MCP3008:
